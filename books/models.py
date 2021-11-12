3f42e4962bf0f4cbc7422c2e8c7dca19e591607b
pacisauctor <agarciadarce@gmail.com>
2021-11-12 14:46:38 -0600

    ✨ agregando modelos

--- books/models.py
+++ books/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(verbose_name="Nombres", max_length=50)
+    last_name = models.CharField(verbose_name="Apellidos", max_length=50)
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 class Book(models.Model):
     name = models.CharField(verbose_name="Nombre del libro", max_length=50)
-    author = models.CharField(verbose_name="Autor del libro", max_length=75)
+    author = models.ForeignKey(Author, related_name="books",verbose_name="Autor", on_delete=models.CASCADE)
     price = models.FloatField(verbose_name="Precio")
     preview = models.FileField(upload_to="books/", verbose_name="Adjunto del libro")
 
     def __str__(self):
         return self.name
+
+class Reader(models.Model):
+    first_name = models.CharField(verbose_name="Nombres", max_length=50)
+    last_name = models.CharField(verbose_name="Apellidos", max_length=50)
+    readed_books = models.ManyToManyField(Book, related_name="readers")
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
